d1da887a0bb4a4e3f05657f822a2d1c9b4884bcf
Salim <salimbenahmed14@gmail.com>
2019-01-18 21:14:05 +0100

    my first commit

---
+++ src/main/java/com/example/demowsdl/DemoWsdlApplication.java
@@ -0,0 +1,14 @@
+package com.example.demowsdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWsdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWsdlApplication.class, args);
+    }
+
+}
+
